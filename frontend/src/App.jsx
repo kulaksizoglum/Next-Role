272bac25415ee3e85644f18fd73865f5c5020b48
kulaksizoglum <147839439+kulaksizoglum@users.noreply.github.com>
2026-05-20 21:03:32 +0300

    setup frontend with vite tailwind and daisyui

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <button className="btn btn-primary">
+        DaisyUI Çalışıyor
+      </button>
+    </div>
+  );
+}
+
+
+export default App;
